81bb3ce1c88102accb1b7b87e003e8c02756726b
shinyeon57 <noeys57@sookmyung.ac.kr>
2019-10-02 15:19:18 +0900

    04_ practice

--- main.c
+++ main.c
@@ -4,6 +4,24 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	
+	//practice01
+	
+	int x,y,z,m;
+	int a,b,c;
+
+	x=1;
+	z=3;
+	a=3;
+	b=4;
+	c=5;
+
+	y=a*x*x+b*x+c;
+	m=(x+y+x)/3;
+	printf("y=%d,m=%d",y,m);
+	
+	//practice02
+	
 	int x=3;
 	int cond1, cond2;
 	
@@ -12,5 +30,19 @@ int main(int argc, char *argv[]) {
 	
 	printf("cond1=%d,cond2=%d",
 	cond1,cond2);
-		return 0;
-} 
+	
+	//practice03
+	
+	int x,y;
+	
+	printf("enter 2 integers :");
+	scanf("%d %d", &x, &y);
+	
+	printf("+ is %d\n:",x+y);
+	printf("- is %d\n:",x-y);
+	printf("* is %d\n:",x*y);
+	printf("/ is %d\n:",x/y);
+	printf("%% is %d\n",x%y);
+
+	return 0;
+}
